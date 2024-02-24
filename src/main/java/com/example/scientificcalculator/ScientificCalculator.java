d646fb02ee3c6d9f56c9464e0b78ed20c8617771
tanvi1306 <tanvimotwani.tm@gmail.com>
2024-02-24 14:46:41 +0530

    first commit

---
+++ src/main/java/com/example/scientificcalculator/ScientificCalculator.java
@@ -0,0 +1,68 @@
+package com.example.scientificcalculator;
+
+import java.util.Scanner;
+
+public class ScientificCalculator
+{
+    public double squareRoot(double num)
+    {
+        return Math.sqrt(num);
+    }
+    public int factorial(int num)
+    {
+        int ans = 1;
+        for(int i=2;i<=num;i++)
+        {
+            ans*=i;
+        }
+        return ans;
+    }
+    public double logarithm(double num)
+    {
+        return Math.log(num);
+    }
+    public double expo(double num,double a)
+    {
+        return Math.pow(num,a);
+    }
+    public void display()
+    {
+        Scanner sc= new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Menu :: Scientific Calculator\nChoice 1. Square root\nChoice 2. Factorial\nChoice 3. Natural Logarithm(base e)\nChoice 4. Power\nChoice 5.Exit");
+            System.out.println("Enter Your Choice");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            if(choice==1)
+            {
+                System.out.println("Enter the Number to Calculate Square Root:");
+                System.out.println("Square Root of the Number is:"+squareRoot(sc.nextDouble()));
+            }
+            else if(choice==2)
+            {
+                System.out.println("Enter the Number to Calculate Factorial:");
+                System.out.println("Factorial of the Number is:"+factorial(sc.nextInt()));
+            }
+            else if(choice==3)
+            {
+                System.out.println("Enter the Number to Calculate Natural Logarithm:");
+                System.out.println("Natural Logarithm of the Number is:"+logarithm(sc.nextDouble()));
+            }
+            else if(choice==4)
+            {
+                System.out.println("Enter the Number:");
+                double num = sc.nextDouble();
+                sc.nextLine();
+                System.out.println("Enter the Power:");
+                double pow = sc.nextDouble();
+                System.out.println("Power of the Number is:"+expo(num,pow));
+            }
+            else
+            {
+                System.out.println("Exit");
+                System.exit(0);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/scientificcalculator/ScientificCalculatorApplication.java
@@ -0,0 +1,10 @@
+package com.example.scientificcalculator;
+
+public class ScientificCalculatorApplication {
+
+    public static void main(String[] args) {
+        ScientificCalculator sc = new ScientificCalculator();
+        sc.display();
+    }
+
+}
