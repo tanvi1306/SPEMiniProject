e323fe243b77c407f12593a2db7f184e921eb2f3
tanvi1306 <tanvimotwani.tm@gmail.com>
2024-02-24 16:49:13 +0530

    Updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,6 +19,11 @@ pipeline {
             }
         }
 
+        stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+
         stage('Build Docker Image') {
             steps {
                 script {
--- src/test/java/com/example/scientificcalculator/ScientificCalculatorApplicationTests.java
+++ src/test/java/com/example/scientificcalculator/ScientificCalculatorApplicationTests.java
@@ -3,11 +3,63 @@ package com.example.scientificcalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class ScientificCalculatorApplicationTests {
 
+    //sqr root
     @Test
-    void contextLoads() {
+    public void testSquareRootForZero() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        assertEquals(0.0, calculator.squareRoot(0), 0.001);
+    }
+
+    @Test
+    public void testSquareRootForNegativeNumber() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        // Assuming that squareRoot function returns NaN for negative numbers
+        assertTrue(Double.isNaN(calculator.squareRoot(-4)));
+    }
+
+    //factorial
+    @Test
+    public void testFactorialForZero() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        assertEquals(1, calculator.factorial(0));
+    }
+
+    @Test
+    public void testFactorialForNegativeNumber() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        // Assuming that factorial function throws an IllegalArgumentException for negative numbers
+        assertThrows(IllegalArgumentException.class, () -> calculator.factorial(-4));
+    }
+
+    @Test
+    public void testLogarithmForZero() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        // Assuming that logarithm function returns negative infinity for zero
+        assertEquals(Double.NEGATIVE_INFINITY, calculator.logarithm(0), 0.001);
+    }
+
+    @Test
+    public void testLogarithmForNegativeNumber() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        // Assuming that logarithm function throws an IllegalArgumentException for negative numbers
+        assertThrows(IllegalArgumentException.class, () -> calculator.logarithm(-4));
+    }
+
+    @Test
+    public void testExpoForZeroBase() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        assertEquals(0.0, calculator.expo(0, 2), 0.001);
+    }
+
+    @Test
+    public void testExpoForZeroExponent() {
+        ScientificCalculator calculator = new ScientificCalculator();
+        assertEquals(1.0, calculator.expo(4, 0), 0.001);
     }
 
 }
