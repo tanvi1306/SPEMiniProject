15b4a68e6469baaafac2bed6912b15488803a319
tanvi1306 <tanvimotwani.tm@gmail.com>
2024-02-24 16:01:03 +0530

    Updated ScientificCalculatorApplication.java

--- src/main/java/com/example/scientificcalculator/ScientificCalculatorApplication.java
+++ src/main/java/com/example/scientificcalculator/ScientificCalculatorApplication.java
@@ -1,5 +1,8 @@
 package com.example.scientificcalculator;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ScientificCalculatorApplication {
 
     public static void main(String[] args) {
