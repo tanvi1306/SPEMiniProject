79b6327866d3579bf97660cdeb75c3b56e3c2b1c
tanvi1306 <tanvimotwani.tm@gmail.com>
2024-02-24 17:21:54 +0530

    Updated

--- src/main/java/com/example/scientificcalculator/ScientificCalculator.java
+++ src/main/java/com/example/scientificcalculator/ScientificCalculator.java
@@ -41,39 +41,36 @@ public class ScientificCalculator
         Scanner sc= new Scanner(System.in);
         while(true)
         {
-            System.out.println("Menu :: Scientific Calculator\nChoice 1. Square root\nChoice 2. Factorial\nChoice 3. Natural Logarithm(base e)\nChoice 4. Power\nChoice 5.Exit");
-            System.out.println("Enter Your Choice");
-            int choice = sc.nextInt();
-            sc.nextLine();
-            if(choice==1)
-            {
-                System.out.println("Enter the Number to Calculate Square Root:");
-                System.out.println("Square Root of the Number is:"+squareRoot(sc.nextDouble()));
-            }
-            else if(choice==2)
-            {
-                System.out.println("Enter the Number to Calculate Factorial:");
-                System.out.println("Factorial of the Number is:"+factorial(sc.nextInt()));
-            }
-            else if(choice==3)
-            {
-                System.out.println("Enter the Number to Calculate Natural Logarithm:");
-                System.out.println("Natural Logarithm of the Number is:"+logarithm(sc.nextDouble()));
-            }
-            else if(choice==4)
-            {
-                System.out.println("Enter the Number:");
-                double num = sc.nextDouble();
+            try {
+                System.out.println("Menu :: Scientific Calculator\nChoice 1. Square root\nChoice 2. Factorial\nChoice 3. Natural Logarithm(base e)\nChoice 4. Power\nChoice 5.Exit");
+                System.out.println("Enter Your Choice");
+                int choice = sc.nextInt();
                 sc.nextLine();
-                System.out.println("Enter the Power:");
-                double pow = sc.nextDouble();
-                System.out.println("Power of the Number is:"+expo(num,pow));
+                if (choice == 1) {
+                    System.out.println("Enter the Number to Calculate Square Root:");
+                    System.out.println("Square Root of the Number is:" + squareRoot(sc.nextDouble()));
+                } else if (choice == 2) {
+                    System.out.println("Enter the Number to Calculate Factorial:");
+                    System.out.println("Factorial of the Number is:" + factorial(sc.nextInt()));
+                } else if (choice == 3) {
+                    System.out.println("Enter the Number to Calculate Natural Logarithm:");
+                    System.out.println("Natural Logarithm of the Number is:" + logarithm(sc.nextDouble()));
+                } else if (choice == 4) {
+                    System.out.println("Enter the Number:");
+                    double num = sc.nextDouble();
+                    sc.nextLine();
+                    System.out.println("Enter the Power:");
+                    double pow = sc.nextDouble();
+                    System.out.println("Power of the Number is:" + expo(num, pow));
+                } else {
+                    System.out.println("Exit");
+                    System.exit(0);
+                }
             }
-            else
-            {
-                System.out.println("Exit");
-                System.exit(0);
-            }
+            catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            sc.nextLine(); // Clear the input buffer
+        }
         }
     }
 }
